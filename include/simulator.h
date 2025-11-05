e7b51a9367bef5995a84eb8f124c7e01f58fb965
Lucas Rezende <66080424+Lucas-Rezende@users.noreply.github.com>
2025-11-05 15:12:40 -0300

    Add Frame (initial version)

--- include/simulator.h
+++ include/simulator.h
@@ -9,4 +9,13 @@ typedef struct {
     size_t upper_value;
 } ReasonableRange;
 
+/* Since the operating system is managing physical memory, it must be aware of the allocation details of physical memory—which frames are allocated, which frames are available, how many total frames there are, and so on. This information is generally kept in a single, system-wide data structure called a frame table.*/
+typedef struct {
+    int page_id;            
+    bool dirty_bit;         
+    long last_access_time;
+} Frame;
+
+unsigned calcular_shift_bits(int tamanho_pagina_kb);
+
 #endif
