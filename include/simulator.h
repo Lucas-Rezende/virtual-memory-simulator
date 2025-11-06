e5d189c693525da0efb6039a70c6e11280fb0292
Lucas Rezende <66080424+Lucas-Rezende@users.noreply.github.com>
2025-11-06 14:29:54 -0300

    Add new functions

--- include/simulator.h
+++ include/simulator.h
@@ -1,21 +1,50 @@
-#ifndef SIMULATOR
-#define SIMULATOR
+#ifndef SIMULATOR_H
+#define SIMULATOR_H
 
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define ERROR -1 
+#define INVALID_PAGE -1 
 #define ARGS_ESPERADOS 5
 #define ARGS_COM_DEBUG 6
 
 typedef struct {
-    size_t lower_value;
-    size_t upper_value;
-} ReasonableRange;
+    char* algoritmo_nome;
+    char* arquivo_log;
+    int tamanho_pagina_kb;
+    int tamanho_memoria_kb;
+    bool debug_mode;
+    
+    int total_frames;
+    unsigned shift_bits;
+} Config;
 
-/* Since the operating system is managing physical memory, it must be aware of the allocation details of physical memory—which frames are allocated, which frames are available, how many total frames there are, and so on. This information is generally kept in a single, system-wide data structure called a frame table.*/
 typedef struct {
     int page_id;            
     bool dirty_bit;         
     long last_access_time;
 } Frame;
 
-unsigned calcular_shift_bits(int tamanho_pagina_kb);
+typedef struct {
+    long total_acessos;
+    long paginas_lidas; // Equivalente a Page Faults
+    long paginas_escritas;
+} Stats;
+
+
+unsigned calcular_shift_bits(int tamanho_pagina_bytes);
+
+void simular_acesso(
+    Frame* memoria_fisica, 
+    Config* config,
+    Stats* stats,
+    unsigned page_number, 
+    char rw, 
+    long current_time
+);
+
+// TODO: Aqui seriam adicionados os eventuais algoritmos e algum método auxiliar para decidir qual
+// seria usado, dependendo do input.
 
 #endif
